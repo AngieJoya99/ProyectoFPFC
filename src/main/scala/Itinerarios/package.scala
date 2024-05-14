3b98fed7ae1ed3ad0be7cfe4df4f53b2aaba345c
AngieJoya99 <angie.joya@correounivalle.edu.co>
2024-05-14 14:55:04 -0500

    Avances

---
+++ src/main/scala/Itinerarios/package.scala
@@ -0,0 +1,8 @@
+/*Angie Joya - 2322609
+Johan David Pitto - 1932739
+Miguel Ángel Salcedo - 2242786
+José Daniel Trujillo - 2225611*/
+
+package object Itinerarios{
+    
+}
---
+++ src/main/scala/ItinerariosPar/package.scala
@@ -0,0 +1,11 @@
+/*Angie Joya - 2322609
+Johan David Pitto - 1932739
+Miguel Ángel Salcedo - 2242786
+José Daniel Trujillo - 2225611*/
+
+import common._
+import scala.collection.parallel.CollectionConverters._
+
+package object ItinerariosPar{
+    
+}
