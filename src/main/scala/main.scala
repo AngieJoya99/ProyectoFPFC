bcffc2b8837b5e7b44169f17992f83714bb08d1c
AngieJoya99 <angie.joya@correounivalle.edu.co>
2024-05-16 09:36:53 -0500

    Avances

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,4 +1,11 @@
-@main
-def main(): Unit = {
-  println("Hello world!")
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    import Itinerarios._
+    import ItinerariosPar._
+    import Datos._
+    import common._
+    
+    println("Hello, world!")
+  }
 }
