40e6f8778442a9714cbffbd5ce131ea90e63745a
MiguelSalcedo225 <salcedo.miguel@correounivalle.edu.co>
2024-05-17 20:20:59 -0500

    Update package.scala

--- src/main/scala/Itinerarios/package.scala
+++ src/main/scala/Itinerarios/package.scala
@@ -26,18 +26,16 @@ package object Itinerarios{
     * todos los itinerarios entre los dos aeropuertos
     */
   def itinerarios(vuelos: List[Vuelo], aeropuertos: List[Aeropuerto]): (String, String) => List[Itinerario] = {
-    def buscarItinerarios(cod1: String, cod2: String): List[Itinerario] = {
-      def generarItinerario(cod1: String, cod2: String, visitados: Set[String], vuelosRestantes: List[Vuelo]): List[Itinerario] = vuelosRestantes.flatMap {
-        case vuelo if vuelo.Org == cod1 && vuelo.Dst == cod2 && !visitados.contains(vuelo.Dst) =>
-          List(List(vuelo))
-        case vuelo if vuelo.Org == cod1 && !visitados.contains(vuelo.Dst) =>
-          val nuevosVisitados = visitados + vuelo.Org
-          generarItinerario(vuelo.Dst, cod2, nuevosVisitados, vuelos.filterNot(_ == vuelo)).map(vuelo :: _)
-        case _ => None
-      }
-      generarItinerario(cod1, cod2, Set.empty, vuelos)
-    }    
-    buscarItinerarios
+    def generarItinerario(cod1: String, cod2: String, visitados: Set[String], vuelosRestantes: List[Vuelo]): List[Itinerario] = vuelosRestantes.flatMap {
+      case vuelo if vuelo.Org == cod1 && vuelo.Dst == cod2 && !visitados.contains(vuelo.Dst) =>
+        List(List(vuelo))
+      case vuelo if vuelo.Org == cod1 && !visitados.contains(vuelo.Dst) =>
+        val nuevosVisitados = visitados + vuelo.Org
+        generarItinerario(vuelo.Dst, cod2, nuevosVisitados, vuelos.filterNot(_ == vuelo)).map(vuelo :: _)
+      case _ => List.empty
+    }
+
+    (cod1: String, cod2: String) => generarItinerario(cod1, cod2, Set.empty, vuelos)
   }
 
   /** Dada una lista de todos los vuelos disponibles y una lista 
